360e7b5725ec3405505268570e263442a66eb47a
roceleylaw <roceleylaw@gmail.com>
2018-11-05 06:54:59 -0800

    Fixed callback errors and create a new route that get all values from a table

--- serverCode/api/routes/routes.js
+++ serverCode/api/routes/routes.js
@@ -1,14 +1,21 @@
 'use strict';
 //export by default
+var task=require('../controllers/taskController');
 module.exports = function (app) {
+    //get all persons at route baseURL/allpersons 
+    app.route('/allpersons')
+        .get(function (req, res, next) {
+            task.getAllPersons(function(err, result){
+                if (err){
+                    res.json(err);
+                } else {
+                    res.json(result);
+                }
+            });
+        });
     
-    // EXAMPLES: the coding example have been given below as guidelines
-    // grab 
-    // var strain = require('../controllers/controller');
-    // var siteRoot = require('../routes/routes');
-
-
-    // Strains
-    // app.route('/strains')
-    //     .get(strain.getAllItems);
+    // TODO: add routes
+    app.route('/').get(function (req, res, next) {
+        res.send('Welcome to the our API');
+    });
 }
