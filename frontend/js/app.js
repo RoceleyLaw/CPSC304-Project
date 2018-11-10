a6f0ad6a20027e774dd38de1a8ae5aa31f57082d
Sean MacDonald <seandoylemacdonald@gmail.com>
2018-11-09 16:19:17 -0800

    frontend first commit

---
+++ frontend/js/app.js
@@ -0,0 +1,53 @@
+//testing request
+
+var ROUTE_URL = "https://cpen400a-bookstore.herokuapp.com/products"; 
+
+var ajaxGet = function(url, onSuccess, onError){
+    var xhttp = new XMLHttpRequest();
+    xhttp.timeout = 3000; // timeout set to 2 seconds 
+    var sendCount = 0; 
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var products = JSON.parse(xhttp.responseText); //convert payload to object
+            onSuccess(products); 
+        } else if(this.readyState == 4 && this.status == 500) {
+            if(sendCount > 2) {
+                onError(xhttp.responseText); 
+            } else {
+                sendCount++; 
+                //send request again 
+                xhttp.open("GET", url, true);
+                xhttp.send();
+            }
+        }
+    };
+
+    xhttp.ontimeout = function (error) {
+        console.log("TIMEOUT");
+        console.log(error); 
+        if(sendCount > 2) {
+            onError("Timeout Occurred"); 
+        } else {
+            sendCount++; 
+            //send request again 
+            xhttp.open("GET", url, true);
+            xhttp.send();
+        }
+      };
+    
+    //send the request for the first time
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+
+window.onload = function() {
+    ajaxGet(ROUTE_URL, 
+        function(testObject){
+            console.log(testObject);
+        },
+        function(error){
+            console.log(error);
+        }); 
+}
