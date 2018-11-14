770d6d5556fde1850e7cc76ea658869414be40fe
Sean MacDonald <seandoylemacdonald@gmail.com>
2018-11-13 16:36:50 -0800

    realtor id is now saved and passed on every page

--- frontend/js/realtor/realtorListings.js
+++ frontend/js/realtor/realtorListings.js
@@ -1,3 +1,6 @@
+//GLOBAL VARIABLES 
+var REALTOR_NUMBER; 
+
 //testing request
 
 var ROUTE_URL = "https://evening-fjord-94245.herokuapp.com";
@@ -300,6 +303,33 @@ function deleteListings(id){
 
 window.onload = function() {
     console.log('Hello')
+
+    //GET REALTOR NUMBER
+    var url_string = window.location.href
+    var url = new URL(url_string);
+    REALTOR_NUMBER = url.searchParams.get("id");
+    console.log("Client phone number: " + REALTOR_NUMBER); 
+
+    //realtorListings
+    var url1 = "./realtorListings.html?id=" + REALTOR_NUMBER; 
+    var element1 = document.getElementById('menuHeaders1');
+    element1.setAttribute("href",url1)
+
+    //soldListings 
+    var url2 = "./soldListings.html?id=" + REALTOR_NUMBER; 
+    var element2 = document.getElementById('menuHeaders2');
+    element2.setAttribute("href",url2);
+
+    //realtorAppointments 
+    var url3 = "./realtorAppointments.html?id=" + REALTOR_NUMBER; 
+    var element3 = document.getElementById('menuHeaders3');
+    element3.setAttribute("href",url3);
+
+    //realorSettings
+    var url4 = "./realtorSettings.html?id=" + REALTOR_NUMBER; 
+    var element4 = document.getElementById('menuHeaders4');
+    element4.setAttribute("href",url4);
+
     ajaxGet(ROUTE_URL + ALL_POSTS , 
         function(testObject){
             console.log("GET SUCCESS"); 
