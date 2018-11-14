33d57781abc23e57524bbcd253d29cd87509a223
charn_rai23 <charn_rai23@server.ca>
2018-11-13 22:55:22 -0800

    both client and realtor signup, settings page work

--- frontend/js/client/clientSettings.js
+++ frontend/js/client/clientSettings.js
@@ -1,5 +1,83 @@
 //GLOBAL VARIABLES 
 var CLIENT_PHONE_NUMBER; 
+var ROUTE_URL = "https://evening-fjord-94245.herokuapp.com";
+var GET = "/allClients/"; 
+var PUT ="/allClients/:phoneNumber?";
+var ajaxGet = function(url, onSuccess, onError){
+    var xhttp = new XMLHttpRequest();
+    xhttp.timeout = 3000; // timeout set to 2 seconds 
+    var sendCount = 0; 
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var products = JSON.parse(xhttp.responseText); //convert payload to object
+            onSuccess(products); 
+        } else if(this.readyState == 4 && this.status == 500) {
+            if(sendCount > 2) {
+                onError(xhttp.responseText); 
+            } else {
+                sendCount++; 
+                //send request again 
+                xhttp.open("GET", url, true);
+                xhttp.send();
+            }
+        }
+    };
+
+    xhttp.ontimeout = function (error) {
+        console.log("TIMEOUT");
+        console.log(error); 
+        if(sendCount > 2) {
+            onError("Timeout Occurred"); 
+        } else {
+            sendCount++; 
+            //send request again 
+            xhttp.open("GET", url, true);
+            xhttp.send();
+        }
+      };
+    
+    //send the request for the first time
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+var ajaxPut = function(url, onSuccess, onError,jsonString){
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.open("PUT", url, true); 
+
+    xhttp.setRequestHeader("Content-Type", "application/json"); 
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var responseObject = JSON.parse(xhttp.responseText); 
+            onSuccess(responseObject); 
+        } else if (this.readyState == 4 && this.status == 500) {
+            onError(xhttp.responseText); 
+        }
+    }
+
+    xhttp.send(jsonString); 
+}
+
+function updateClientInformation(){
+    console.log("hello");
+    var obj = new Object();
+    obj.clientName = document.getElementById("lg1").value;
+    obj.phoneNumber = document.getElementById("lg2").value;
+    obj.clientEmail = document.getElementById("lg3").value;
+    //obj.password = document.getElementById("inputPassword").value;
+                
+    var newJSON = JSON.stringify(obj);
+    console.log(newJSON);
+    ajaxPut("https://evening-fjord-94245.herokuapp.com/allClients/" + CLIENT_PHONE_NUMBER, function(){
+        console.log("POST Success"); 
+        location.reload();
+    }, 
+    function(error){
+        console.log("POST ERROR"); 
+        console.log(error); 
+    }, newJSON);
+}
 
 window.onload = function() {
     console.log('Hello')
@@ -23,4 +101,18 @@ window.onload = function() {
     var url3 = "./clientSettings.html?id=" + CLIENT_PHONE_NUMBER; 
     var element3 = document.getElementById('menuHeaders3');
     element3.setAttribute("href",url3);
+
+    ajaxGet(ROUTE_URL + GET + CLIENT_PHONE_NUMBER, 
+        function(testObject){
+            console.log("GET SUCCESS");            
+            console.log(testObject);
+            document.getElementById("lg1").value=testObject[0].clientName;
+            document.getElementById("lg2").value=testObject[0].phoneNumber;
+            document.getElementById("lg3").value=testObject[0].clientEmail;
+            
+        },
+        function(error){
+            console.log("GET ERROR"); 
+            console.log(error);
+        }); 
 }
