ee0401fe8ca9673624e4002d79c98c5b727ae3b5
charn_rai23 <charn_rai23@server.ca>
2018-11-16 11:02:31 -0800

    started appointments

--- frontend/js/client/clientAppointments.js
+++ frontend/js/client/clientAppointments.js
@@ -1,6 +1,120 @@
 //GLOBAL VARIABLES 
 var CLIENT_PHONE_NUMBER; 
 
+var ROUTE_URL = "https://evening-fjord-94245.herokuapp.com";
+var ALL_REALTORS = "/allRealtors"
+
+
+var ajaxGet = function(url, onSuccess, onError){
+    var xhttp = new XMLHttpRequest();
+    xhttp.timeout = 3000; // timeout set to 2 seconds 
+    var sendCount = 0; 
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var products = JSON.parse(xhttp.responseText); //convert payload to object
+            onSuccess(products); 
+        } else if(this.readyState == 4 && this.status == 500) {
+            if(sendCount > 2) {
+                onError(xhttp.responseText); 
+            } else {
+                sendCount++; 
+                //send request again 
+                xhttp.open("GET", url, true);
+                xhttp.send();
+            }
+        }
+    };
+
+    xhttp.ontimeout = function (error) {
+        console.log("TIMEOUT");
+        console.log(error); 
+        if(sendCount > 2) {
+            onError("Timeout Occurred"); 
+        } else {
+            sendCount++; 
+            //send request again 
+            xhttp.open("GET", url, true);
+            xhttp.send();
+        }
+      };
+    
+    //send the request for the first time
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+var ajaxPost = function(url, onSuccess, onError, jsonString){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", url, true);
+
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    //xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhttp.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            // Request finished. Do processing here.
+            var products = JSON.parse(xhttp.responseText); //convert payload to object
+            onSuccess(products);
+        } else if(this.readyState == 4 && this.status == 500) {
+                onError(xhttp.responseText); 
+        }
+    }
+
+    //xhttp.send("foo=bar&lorem=ipsum"); 
+    xhttp.send(jsonString); 
+}
+
+var ajaxPut = function(url, onSuccess, onError, putJSON){
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.open("PUT", url, true); 
+
+    xhttp.setRequestHeader("Content-Type", "application/json"); 
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var responseObject = JSON.parse(xhttp.responseText); 
+            onSuccess(responseObject); 
+        } else if (this.readyState == 4 && this.status == 500) {
+            onError(xhttp.responseText); 
+        }
+    }
+
+    xhttp.send(JSON.stringify(putJSON)); 
+}
+
+var ajaxDelete = function(url, onSuccess, onError) {
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.open("DELETE", url, true); 
+
+    xhttp.setRequestHeader("Content-Type", "application/json"); 
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var responseObject = JSON.parse(xhttp.responseText); 
+            onSuccess(responseObject); 
+        } else if (this.readyState == 4 && this.status == 500) {
+            onError(xhttp.responseText); 
+        }
+    }
+
+    xhttp.send(null); 
+}
+
+
+var createDropDown = function(){
+    var options = document.getElementById("options");
+    ajaxGet(ROUTE_URL + ALL_REALTORS, 
+        function(testobject){
+            
+    },
+        function(error){
+        console.log("ERROR")
+    })
+
+}
+
+
 window.onload = function() {
     console.log('Hello')
 
